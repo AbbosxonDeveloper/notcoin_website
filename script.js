258dde124456b0727d99b56ba8fe8238a10bf61a
AbbosxonDeveloper <abbosxonoripov3@gmail.com>
2024-03-03 16:31:05 +0500

    add coins storage

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let coin = document.getElementById("coin");
+let main_amount = document.getElementById("main_amount");
+// main_amount.textContent = 999989
+
+let getCoin = JSON.parse(localStorage.getItem("coin"));
+let storageCash = localStorage.setItem("coin", getCoin || '0');
+
+main_amount.textContent = getCoin | 0
+
+console.log(getCoin);
+main_amount.textContent = numberWithCommas(main_amount.textContent)
+
 function numberWithCommas(x) {
     x = x.toString();
     var pattern = /(-?\d+)(\d{3})/;
@@ -6,13 +18,10 @@ function numberWithCommas(x) {
     return x;
 }
 
-let coin = document.getElementById("coin")
-let main_amount = document.getElementById("main_amount")
-main_amount.textContent = 999989
-main_amount.textContent = numberWithCommas(main_amount.textContent)
-
 coin.onclick = () => {
-    let toNumber = +main_amount.textContent.split(',').join('')
-    toNumber += 1;
-    main_amount.textContent = numberWithCommas(toNumber)
+    let toNumber = +main_amount.textContent.split(',').join('');
+    getCoin += 1;
+    console.log(getCoin);
+    localStorage.setItem("coin", JSON.stringify(getCoin));
+    main_amount.textContent = numberWithCommas(getCoin);
 }
